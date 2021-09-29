fad328cbc51dcdf620ae3161315c4aa3ced73f41
annasimanjuntak <anna.sulastri@peruridigit.co.id>
2021-09-29 10:01:41 +0700

    delete role from table users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'role' => ['required', 'string'],
+           
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'captcha' => ['required','captcha'],
@@ -74,7 +74,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'role'=>$data['role'],
+           
             'password' => Hash::make($data['password']),
         ]);
     }
